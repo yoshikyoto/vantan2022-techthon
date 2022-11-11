e500d5898f3452316ae5237532e0dfa2e7a2c534
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2022-11-11 10:35:32 +0900

    initil commit

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from django.views import View
+# JsonResponse を import
+from django.http import JsonResponse
+
+class CheckView(View):
+    def get(self, request):
+        python_dict = {
+            "status_code": 200,
+            "method": "GET",
+        }
+        return JsonResponse(python_dict)
